99276c4b9f64652b02b13155f9fcae8281d8bc9f
mohammadsalmani28 <mohammadsalmani28@gmail.com>
2020-01-15 16:52:09 +0330

    Create Aparat.php

---
+++ src/Facades/Aparat.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Mohammadsalmani28\Aparat\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+
+class Aparat extends Facade
+{
+	
+	protected static function getFacadeAccessor(){
+
+		return 'Aparat';
+	}
+}
